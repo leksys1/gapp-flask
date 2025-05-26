e2b9d13e2e09548ebce0ca1959909edc6a909820
leksys1 <sevillenosixela@gmail.com>
2025-05-26 11:04:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.getElementById('predictionForm').addEventListener('submit', async funct
     };
 
     try {
-        const response = await fetch('http://localhost:8000/api/hfp_prediction', {
+        const response = await fetch('https://gapp-flask.onrender.com/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ inputs: [inputData] })
